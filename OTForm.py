8219724feee4e069e628d61add88c1b388701544
Chou KuangMao <u9113034@gmail.com>
2023-10-02 17:37:38 +0800

    Add files via upload

--- OTForm.py
+++ OTForm.py
@@ -1,6 +1,12 @@
 import streamlit as st
 import datetime
-import pyperclip
+import clipboard
+
+
+def on_copy_click(text):
+    # st.session_state.copied.append(text)
+    clipboard.copy(text)
+
 
 sites = ('高雄', '公司', '南科', '台中', '美光', '新竹', '林口')
 st.title(':male-mechanic:裝機輸入表單:male-mechanic:')
@@ -11,6 +17,8 @@ got = st.time_input('出發時間', datetime.time(9, 0), step=300)
 ont = st.time_input('進廠時間', datetime.time(9, 30), step=300)
 offt = st.time_input('離廠時間', datetime.time(18, 0), step=300)
 ot = st.number_input('OT', min_value=0.0, step=0.5)
+if ot == 0.0:
+    ot = ''
 lunch = st.radio('餐費', [100, 200], index=0)
 night = st.checkbox('過夜')
 night = 1 if night else ''
@@ -20,7 +28,7 @@ else:
     hotel = ''
 # open staff csv ile
 slist = []
-with open('staff.txt', 'r', encoding='UTF-8' ) as file_object:
+with open('staff.txt', 'r', encoding='UTF-8') as file_object:
     staff = file_object.readlines()
     for s in staff:
         slist.append(s.strip('\n'))
@@ -30,27 +38,26 @@ with open('staff.txt', 'r', encoding='UTF-8' ) as file_object:
 
 col1, col2, col3 = st.columns(3)
 with col1:
-   site1 = st.selectbox('地點1', sites, index=1, key='公司')
+    site1 = st.selectbox('地點1', sites, index=1, key='公司')
 with col2:
-   site2 = st.selectbox('地點2', sites, index=2, key='南科')
+    site2 = st.selectbox('地點2', sites, index=2, key='南科')
 with col3:
-   way = st.radio('', ['去程', '回程', '來回'], label_visibility='collapsed')
+    way = st.radio('', ['去程', '回程', '來回'], label_visibility='collapsed')
 remark = st.text_area('備註')
 st.divider()
 content = st.text_area('輸出OT字串',
-             f'{title}\n'
-             f'日期:{d.strftime("%m/%d")}\n'
-             f'出發:{got.strftime("%H:%M")}\n'
-             f'進廠:{ont.strftime("%H:%M")}\n'
-             f'離廠:{offt.strftime("%H:%M")}\n'
-             f'OT:{ot}\n'
-             f'餐費:{lunch}\n'
-             f'住宿:{night}\n'
-             f'坐車:{siton}\n'
-             f'開車:{drivers}\n'
-             f'自行前往:{goself}\n'
-             f'里程:{site1}-{site2} {way}\n'
-             f'備註:{remark}\n', height=330)
-if st.button('Copy'):
-    pyperclip.copy(content)
-    st.success('Text copied successfully!')
+                       f'{title}\n'
+                       f'日期:{d.strftime("%m/%d")}\n'
+                       f'出發:{got.strftime("%H:%M")}\n'
+                       f'進廠:{ont.strftime("%H:%M")}\n'
+                       f'離廠:{offt.strftime("%H:%M")}\n'
+                       f'OT:{ot}\n'
+                       f'餐費:{lunch}\n'
+                       f'住宿:{night}\n'
+                       f'坐車:{siton}\n'
+                       f'開車:{drivers}\n'
+                       f'自行前往:{goself}\n'
+                       f'里程:{site1}-{site2} {way}\n'
+                       f'備註:{remark}\n', height=330)
+st.button("📋複製字串", on_click=on_copy_click, args=(content,))
+st.success('Text copied successfully!')
